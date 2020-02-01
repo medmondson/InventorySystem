14af92af8e862a7cf91b924844e6e01fc39d225d
suresh rathinasamy <suresh.rathinasamy@omnibus.uk.com>
2020-02-01 19:26:49 +0000

    Variable name change Changer to By

--- GildedRoseLib/Model/Items.cs
+++ GildedRoseLib/Model/Items.cs
@@ -3,7 +3,7 @@
     public abstract class Item
     {
         public string Name { get; }
-        public virtual int Changer { get; } = -1;
+        public virtual int By { get; } = -1;
         public string Error { get; set; }
         public abstract (int?, int?) Calculate(int? sellIn, int? quality);
         protected Item(string name)
@@ -15,20 +15,20 @@
 
     public class Normal : Item
     {
-        public override int Changer { get; } = -1;
+        public override int By { get; } = -1;
         public Normal(string name) : base(name) { }
         public override (int?, int?) Calculate(int? sellIn, int? quality)
         {
             DecreaseSellIn(ref sellIn);
             if (sellIn < 0)
-                return (sellIn, quality += (2 * Changer));
-            return (sellIn, quality += Changer);
+                return (sellIn, quality += (2 * By));
+            return (sellIn, quality += By);
         }
     }
 
     public class Legendary : Item
     {
-        public override int Changer { get; } = 0;
+        public override int By { get; } = 0;
         public Legendary(string name) : base(name) { }
         public override (int?, int?) Calculate(int? sellIn, int? quality)
         {
@@ -43,19 +43,19 @@
         {
             DecreaseSellIn(ref sellIn);
             if (sellIn < 0)
-                return (sellIn, quality += (4 * Changer));
-            return (sellIn, quality += (2 * Changer));
+                return (sellIn, quality += (4 * By));
+            return (sellIn, quality += (2 * By));
         }
     }
 
     public class AgedBrie : Item
     {
         public AgedBrie(string name) : base(name) { }
-        public override int Changer { get; } = 1;
+        public override int By { get; } = 1;
         public override (int?, int?) Calculate(int? sellIn, int? quality)
         {
             DecreaseSellIn(ref sellIn);
-            return (sellIn, quality += Changer);
+            return (sellIn, quality += By);
         }
     }
 
@@ -68,10 +68,10 @@
             if (sellIn < 0)
                 return (sellIn, 0);
             if (sellIn <= 5)
-                return (sellIn, quality += (3 * Changer));
+                return (sellIn, quality += (3 * By));
             if (sellIn <= 10)
-                return (sellIn, quality += (2 * Changer));
-            return (sellIn, quality += Changer);
+                return (sellIn, quality += (2 * By));
+            return (sellIn, quality += By);
         }
     }
 
@@ -82,8 +82,8 @@
         {
             DecreaseSellIn(ref sellIn);
             if (sellIn < 0)
-                return (sellIn, quality += (2 * Changer));
-            return (sellIn, quality += Changer);
+                return (sellIn, quality += (2 * By));
+            return (sellIn, quality += By);
         }
     }
 }
